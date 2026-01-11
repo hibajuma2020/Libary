0e5910a524e98ef0cf83e4f0de60bf9d4d7395f1
hiba.js <hiba.js@MEMLT0037>
2026-01-11 11:47:07 +0400

    modified:      //seed data

--- Program.cs
+++ Program.cs
@@ -16,9 +16,9 @@
 
             //seed data
             titles[0] = "Math";
-            authors[0] = "Hiba";
+            authors[0] = "Amjed";
             isbns[0] = "ISBN001";
-            isAvailable[0] =true;
+            isAvailable[0] = true;
             borrowers[0] = "";
 
             lastBookIndex++;
@@ -31,6 +31,64 @@
 
             lastBookIndex++;
 
+            while (true)
+            {
+                Console.WriteLine("Welcome to libary dystem");
+                Console.WriteLine("1. Add New Book");
+                Console.WriteLine("2. Borrow Book");
+                Console.WriteLine("3. Return Book");
+                Console.WriteLine("4. Search Book");
+                Console.WriteLine("5. List Available Books");
+                Console.WriteLine("6. Transfer Book");
+                Console.WriteLine("7. Exit");
+                Console.Write("Choose an option: ");
+
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+
+                    case 1:
+                        //add new book
+                        Console.Write("Enter book title: ");
+                        titles[lastBookIndex + 1] = Console.ReadLine();
+
+                        Console.Write("Enter author name: ");
+                        authors[lastBookIndex + 1] = Console.ReadLine();
+
+                        Console.Write("Enter ISBN: ");
+                        isbns[lastBookIndex + 1] = Console.ReadLine();
+
+                        isAvailable[lastBookIndex + 1] = true;
+                        borrowers[lastBookIndex + 1] = "";
+
+                        lastBookIndex++;
+
+                        Console.WriteLine("Book added successfully!");
+                        break;
+
+                        break;
+
+
+
+
+
+
+
+
+                        break;
+                    defualt:
+                        break;
+
+
+
+                }
+
+
+            }
+
         }
     }
 }
+
+
